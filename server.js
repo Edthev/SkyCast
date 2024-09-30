161a3238ec59aa087745677545c6c013ef95acec
Asden <edthevargas@gmail.com>
2024-09-30 12:21:13 -0400

    add boilerplate express server with dotenv

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const weather = require("data.js");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+   res.send("Server Online");
+});
+
+app.listen(PORT, () => {
+   console.log(`Server Running on http://localhost:${PORT}`);
+});
