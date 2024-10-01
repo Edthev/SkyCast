c1111af64f67f91c14624ee75e7cd9f91536d1ca
Asden <edthevargas@gmail.com>
2024-10-01 11:20:54 -0400

    add minutecast module and dotenv parameters

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-
+const Minute = require("./components/minute");
 dotenv.config();
+
 const PORT = process.env.PORT || 8080;
+const MINUTECAST_API_KEY = process.env.MINUTECAST_API_KEY;
+const CORD_LAT = process.env.CORD_LAT;
+const CORD_LONG = process.env.CORD_LONG;
+
 app.get("/", (req, res) => {
    res.send("Server Online");
+});
+
+app.get("/minute", async (req, res) => {
+   Minute(req, res, MINUTECAST_API_KEY, CORD_LAT, CORD_LONG);
 });
 
 app.listen(PORT, () => {
