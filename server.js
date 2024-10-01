5196743b1e327adc0d5f63c1bd4c8ead5314741d
Asden <edthevargas@gmail.com>
2024-10-01 16:15:17 -0400

    add search endpoint and location env

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const Minutecast = require("./components/minute");
+const CitySearch = require("./components/citySearch");
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const MINUTECAST_API_KEY = process.env.MINUTECAST_API_KEY;
+const LOCATION = process.env.LOCATION;
 const CORD_LAT = process.env.CORD_LAT;
 const CORD_LONG = process.env.CORD_LONG;
 
@@ -15,6 +17,10 @@ app.get("/", (req, res) => {
 
 app.get("/minutecast", async (req, res) => {
    Minutecast(req, res, MINUTECAST_API_KEY, CORD_LAT, CORD_LONG);
+});
+
+app.get("/search", async (req, res) => {
+   CitySearch(req, res, MINUTECAST_API_KEY, LOCATION);
 });
 
 app.listen(PORT, () => {
