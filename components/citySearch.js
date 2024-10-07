491bee64239a8ea38a9c882ec9cf8f2a1a60d69b
Asden <edthevargas@gmail.com>
2024-10-07 11:03:22 -0400

    add api request with location encoding

--- components/citySearch.js
+++ components/citySearch.js
@@ -4,9 +4,10 @@ const citySearch = async (req, res, KEY, LOCATION) => {
    try {
       const ENCODED_LOCATION = LOCATION.replace(/  /g, "%20");
       const response = await axios.get(
-         `http://dataservice.accuweather.com/locations/v1/cities/search?${KEY}&q=${ENCODED_LOCATION}`
+         `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${ENCODED_LOCATION}`
       );
-      res.send({ Status: response.status, Data: response.data });
+      console.log("Location Found:", Array(response.data).length);
+      res.send({ Status: response.status, Location: ENCODED_LOCATION, Data: response.data });
    } catch (err) {
       console.error(err);
       res.send({ ERROR: err });
