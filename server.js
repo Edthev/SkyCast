6297ca794463c63246a590380711defbbf99f2ae
Asden <edthevargas@gmail.com>
2024-10-07 11:00:39 -0400

    add core weather api and fix minutecast file import

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const Minutecast = require("./components/minute");
+const Minutecast = require("./components/minutecast");
 const CitySearch = require("./components/citySearch");
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const MINUTECAST_API_KEY = process.env.MINUTECAST_API_KEY;
+const CORE_WEATHER_API_KEY = process.env.CORE_WEATHER_API_KEY;
 const LOCATION = process.env.LOCATION;
 const CORD_LAT = process.env.CORD_LAT;
 const CORD_LONG = process.env.CORD_LONG;
@@ -20,7 +23,7 @@ app.get("/minutecast", async (req, res) => {
 });
 
 app.get("/search", async (req, res) => {
-   CitySearch(req, res, MINUTECAST_API_KEY, LOCATION);
+   CitySearch(req, res, CORE_WEATHER_API_KEY, LOCATION);
 });
 
 app.listen(PORT, () => {
