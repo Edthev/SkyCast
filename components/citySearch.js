e6bd2a46972542c51e8c2fd60ffe5af1d3e4d088
Asden <edthevargas@gmail.com>
2024-10-07 11:20:25 -0400

    fetch from api if not cached otherwise return cache

--- components/citySearch.js
+++ components/citySearch.js
@@ -1,13 +1,25 @@
 const { default: axios } = require("axios");
 
-const citySearch = async (req, res, KEY, LOCATION) => {
+const citySearch = async (req, res, KEY, LOCATION, cachedLocation) => {
    try {
-      const ENCODED_LOCATION = LOCATION.replace(/  /g, "%20");
-      const response = await axios.get(
-         `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${ENCODED_LOCATION}`
-      );
-      console.log("Location Found:", Array(response.data).length);
-      res.send({ Status: response.status, Location: ENCODED_LOCATION, Data: response.data });
+      const cachedLocationKey = cachedLocation.get("LocationKey");
+      if (!cachedLocationKey) {
+         const ENCODED_LOCATION = LOCATION.replace(/  /g, "%20");
+         const response = await axios.get(
+            `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${ENCODED_LOCATION}`
+         );
+         console.log("Location Found:", Array(response.data).length);
+         const cityKey = response.data[0].Key;
+         cachedLocation.set("LocationKey", cityKey);
+         res.send({
+            Status: response.status,
+            Location: cityKey,
+            Data: response.data,
+         });
+      } else {
+         console.log("retreiving location key from cache");
+         res.send({ Location: cachedLocationKey });
+      }
    } catch (err) {
       console.error(err);
       res.send({ ERROR: err });
