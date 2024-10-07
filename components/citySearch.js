1cac0953ab0b63616fb50e77a50f2a3b1507d1ae
Asden <edthevargas@gmail.com>
2024-10-07 12:03:51 -0400

    add cached cords

--- components/citySearch.js
+++ components/citySearch.js
@@ -3,22 +3,30 @@ const { default: axios } = require("axios");
 const citySearch = async (req, res, KEY, LOCATION, cachedLocation) => {
    try {
       const cachedLocationKey = cachedLocation.get("LocationKey");
-      if (!cachedLocationKey) {
+      const cachedCords = cachedLocation.get("Cords");
+      if (!cachedLocationKey && !cachedCords) {
          const ENCODED_LOCATION = LOCATION.replace(/  /g, "%20");
          const response = await axios.get(
             `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${KEY}&q=${ENCODED_LOCATION}`
          );
          console.log("Location Found:", Array(response.data).length);
          const cityKey = response.data[0].Key;
-         cachedLocation.set("LocationKey", cityKey);
+         const cityLat = response.data[0].GeoPosition.Latitude;
+         const cityLong = response.data[0].GeoPosition.Longitude;
+         const ENCODED_CORDS = `${cityLat}%2C${cityLong}`;
+         cachedLocation.mset([
+            { key: "LocationKey", val: cityKey },
+            { key: "Cords", val: ENCODED_CORDS },
+         ]);
          res.send({
             Status: response.status,
             Location: cityKey,
+            Cords: ENCODED_CORDS,
             Data: response.data,
          });
       } else {
          console.log("retreiving location key from cache");
-         res.send({ Location: cachedLocationKey });
+         res.send({ Location: cachedLocationKey, Cords: cachedCords });
       }
    } catch (err) {
       console.error(err);
