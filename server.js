318a3445cc4ef97d86cba14bbd1fa598b86006fa
Asden <edthevargas@gmail.com>
2024-10-09 10:21:09 -0400

    add mail endpoint and params

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const app = express();
 const Minutecast = require("./components/minutecast");
 const CitySearch = require("./components/citySearch");
+const Mail = require("./components/mail");
 const NodeCache = require("node-cache");
 const cachedLocation = new NodeCache();
 const Forecast = require("./components/forecast");
@@ -11,6 +12,9 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const MINUTECAST_API_KEY = process.env.MINUTECAST_API_KEY;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const EMAIL_SENDER = process.env.EMAIL_SENDER;
+const EMAIL_RECEIVER = process.env.EMAIL_RECEIVER;
 const CORE_WEATHER_API_KEY = process.env.CORE_WEATHER_API_KEY;
 const LOCATION = process.env.LOCATION;
 const localhostURL = "http://localhost:" + PORT;
@@ -30,6 +34,19 @@ app.get("/minutecast", async (req, res) => {
 app.get("/forecast", async (req, res) => {
    Forecast(req, res, CORE_WEATHER_API_KEY, PORT);
 });
+app.get("/mail", async (req, res) => {
+   const SUBJECT = "Weather Minutecast & Forecast";
+   try {
+      const response = await axios.get(localhostURL + "/minutecast");
+      const message = response.data.Data.Message;
+      const HTML = `<div>${JSON.stringify(message)}</div>`;
+      Mail(req, res, SENDGRID_API_KEY, EMAIL_RECEIVER, EMAIL_SENDER, SUBJECT, HTML);
+   } catch (err) {
+      console.log("Error mail:", err);
+      const HTML = `<div>${JSON.stringify(err)}</div>`;
+      Mail(req, res, SENDGRID_API_KEY, EMAIL_RECEIVER, EMAIL_SENDER, SUBJECT, HTML);
+   }
+});
 
 app.get("/search", async (req, res) => {
    CitySearch(req, res, CORE_WEATHER_API_KEY, LOCATION, cachedLocation);
@@ -40,11 +57,20 @@ app.listen(PORT, () => {
 });
 
 const getMinutecast = async () => {
-   const res = await axios.get(localhostURL + "/minutecast");
-   const phrase = res.data.Data.Summary.Phrase;
-   console.log(phrase);
+   try {
+      const res = await axios.get(localhostURL + "/minutecast");
+      const timestamp = new Date();
+      if (res.data.Status == 200) {
+         console.log(res.data.Data.Summary.Phrase + " | " + timestamp);
+         return res.data.Data.Summary.Phrase;
+      } else {
+         return res.data.Data.Message;
+      }
+   } catch (err) {
+      return "err";
+   }
 };
-getMinutecast();
+// getMinutecast();
 setInterval(async () => {
-   getMinutecast();
+   console.log(getMinutecast());
 }, 120 * 60 * 1000);
